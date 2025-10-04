c9fa82cda04f23fbbbfa5148e907a8ff4f39d9cc
Pedro Dias <dias.contatodev@gmail.com>
2025-10-04 12:21:08 -0300

    Adicionado a refatoração e corrigindo o problema com a base de dados, agora usaremos o sqlite para testes e posteriormente implementaremos o postgreSQL

---
+++ PROJETO TEA 2.0/backend/prisma/migrations/20251004151827_init/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT,
+    "googleId" TEXT,
+    "picture" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_googleId_key" ON "users"("googleId");
